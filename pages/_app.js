7075fc4ff4f6bcfbf89559d6919726fa164d4632
kizwond <kizwond@gmail.com>
2021-07-06 17:15:29 +0900

    mutation practice

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,12 @@ import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from "@apo
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
+  // headers: [
+  //   { key: "Access-Control-Allow-Credentials", value: "true" },
+  //   { key: "Access-Control-Allow-Origin", value: "*" },
+  //   { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+  //   { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+  // ]
 });
 
 const store = createStore(allReducers, composeWithDevTools());

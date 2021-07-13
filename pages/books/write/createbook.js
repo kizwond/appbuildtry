1f3d71bd26140873fb6825442bb28784f0bcca9f
kizwond <kizwond@gmail.com>
2021-07-13 16:48:28 +0900

    write main page ing

--- pages/books/write/createbook.js
+++ pages/books/write/createbook.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Layout from "../../../components/layout/Layout";
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, Select } from "antd";
 import Footer from "../../../components/index/Footer";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import Link from "next/link";
@@ -10,22 +10,28 @@ import { logIn, logOut } from "../../../redux/actions";
 import Router from "next/router";
 import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useMutation } from "@apollo/client";
 
+const { Option } = Select;
+
 const CreateBookMutation = gql`
-  mutation CreateBookMutation($title: String!) {
-    createMybook(title: $title) {
+  mutation CreateBookMutation($title: String!, $bookcategory_id:String) {
+    mybook_create(title: $title, bookcategory_id:$bookcategory_id) {
       status
       msg
       mybook {
+       book_info {
         title
         user_id
+        bookcategory_id
+        seq_in_category
+       }
       }
     }
   }
 `;
 
 const GetCategory = gql`
-  query {
-    getBookcategory {
+query {
+    bookcategory_get {
       status
       msg
       bookcategory {
@@ -38,28 +44,27 @@ const GetCategory = gql`
 `;
 
 const CreateBookComponent = () => {
-  const [createMybook] = useMutation(CreateBookMutation, { onCompleted: showdata });
-  const { loading, error, categoryData } = useQuery(GetCategory);
-    if(categoryData !== undefined){
-        console.log(categoryData.getBookcategory.bookcategory[0].name)
-    }
+  const [mybook_create] = useMutation(CreateBookMutation, { onCompleted: showdata });
+  const { loading, error, data } = useQuery(GetCategory);
+  const [category, setCategory] = useState(data.bookcategory_get.bookcategory);
   
-  function showdata(data) {
-    console.log("data", data);
-    if (data.createMybook.msg === "책 생성 성공적!") {
+  function showdata(item) {
+    console.log("data", item);
+    if (item.mybook_create.msg === "책 생성 성공적!") {
       alert("책 만들기 성공!!!!");
       // Router.push("/");
-      window.location.reload();
+      window.location.href="/books/write";
     } else {
       alert("뭔가 잘못되었네요. 다시 해봐요.");
     }
   }
 
-  async function postbook(book_title) {
+  async function postbook(book_title, id) {
     try {
-      await createMybook({
+      await mybook_create({
         variables: {
           title: book_title,
+          bookcategory_id: id
         },
       });
     } catch (error) {
@@ -67,10 +72,8 @@ const CreateBookComponent = () => {
     }
   }
   const onFinish = (values) => {
-    const book_title = values.book_title;
-    // const category = values.category;
-
-    postbook(book_title);
+      console.log(values)
+    postbook(values.book_title, values.category_id);
   };
 
   return (
@@ -80,7 +83,20 @@ const CreateBookComponent = () => {
           <Input placeholder="책제목" />
         </Form.Item>
         <Form.Item name="category">
-          <Input placeholder="카테고리" />
+        <Form.Item
+              className="category_select_naming"
+              name='category_id'
+              rules={[{ required: false, message: '카테고리를 선택해 주세요' }]}
+              
+            >
+              <Select style={{ width: "100%" }} placeholder="카테고리를 선택해 주세요." >
+              <Option>카테고리선택</Option>
+                {category && category.map((category)=>(
+                                      <Option key={category._id} value={category._id}>{category.name}</Option>
+                                    )) }
+                                   
+              </Select>
+            </Form.Item>
         </Form.Item>
         <Form.Item>
           <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
--- pages/books/write/index.js
+++ pages/books/write/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../../../components/layout/Layout";
 import { Form, Input, Button } from "antd";
 import Footer from "../../../components/index/Footer";
@@ -9,7 +9,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { logIn, logOut } from "../../../redux/actions";
 import Router from "next/router";
 import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useMutation } from "@apollo/client";
-import CategorySettingModal from '../../../components/books/write/category/CategorySettingModal'
+import CategorySettingModal from "../../../components/books/write/category/CategorySettingModal";
+
 const GetCategory = gql`
   query {
     bookcategory_get {
@@ -21,13 +22,41 @@ const GetCategory = gql`
         seq
       }
     }
+    mybook_get {
+      status
+      msg
+      mybook {
+        book_info {
+          title
+          user_id
+          bookcategory_id
+          seq_in_category
+        }
+      }
+    }
   }
 `;
 
 const WriteComponent = () => {
   const dispatch = useDispatch();
   const { loading, error, data } = useQuery(GetCategory);
-  console.log("category", data);
+  const [category, setCategory] = useState();
+  const [books, setBooks] = useState();
+
+  // const [books, setBooks] = useState(data.bookcategory_get.mybook.bookinfo);
+  console.log(category);
+
+  useEffect(() => {
+    console.log("컴포넌트가 화면에 나타남");
+
+    if (data) {
+      console.log(data);
+      setCategory(data.bookcategory_get.bookcategory);
+      setBooks(data.mybook_get.mybook);
+    }
+    console.log(category);
+    console.log(books);
+  });
 
   const linkStyle = {
     color: "black",
@@ -53,18 +82,53 @@ const WriteComponent = () => {
     <div>
       <LikeBookList />
       <button onClick={showModal}>카테고리 설정</button>
-      <CategorySettingModal isModalVisible={isModalVisible} handleOk={handleOk} handleCancel={handleCancel}/>
+      <CategorySettingModal isModalVisible={isModalVisible} handleOk={handleOk} handleCancel={handleCancel} />
       <Link href="/books/write/createbook">
         <a style={linkStyle}>
           <button>새책만들기</button>
         </a>
       </Link>
+      <BookList category={category} books={books} />
     </div>
   );
 };
 
 const LikeBookList = () => {
-  return <div>book list</div>;
+  return <div>like book list</div>;
+};
+
+const BookList = ({ category, books }) => {
+  if (category) {
+    var categoryList = category.map((category) => (
+      <>
+        <div>{category.name}</div>
+        <ul>
+          <BooksInCategory categoryId={category._id} books={books} />
+        </ul>
+      </>
+    ));
+  }
+
+  return <div>{categoryList}</div>;
+};
+
+const BooksInCategory = ({ books, categoryId }) => {
+  if (books) {
+    var list = books.map((book) => {
+      if (book.book_info.bookcategory_id === categoryId) {
+        return (
+          <>
+            <li>{book.book_info.title}</li>
+          </>
+        );
+      }
+    });
+  }
+  return (
+    <ul>
+      <li>{list}</li>
+    </ul>
+  );
 };
 const Write = () => {
   return (

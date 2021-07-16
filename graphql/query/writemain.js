d23f4b68af6af5f2fe9f458568cdbe46e2d6a89c
kizwond <kizwond@gmail.com>
2021-07-16 17:26:26 +0900

    ing

---
+++ graphql/query/writemain.js
@@ -0,0 +1,130 @@
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useMutation } from "@apollo/client";
+
+export const GetCategory = gql`
+  query {
+    mybookcate_get {
+      status
+      msg
+      mybookcates {
+        _id
+        mybookcate_info {
+          user_id
+          name
+          seq
+        }
+      }
+    }
+    mybook_get {
+      status
+      msg
+      mybooks {
+        _id
+        mybook_info {
+          title
+          type
+          user_id
+          mybookcate_id
+          seq_in_category
+          hide_or_show
+          studylike
+          writelike
+          seq_in_studylike
+          seq_in_writelike
+        }
+      }
+    }
+  }
+`;
+
+export const BookDeleteMutation = gql`
+  mutation BookDeleteMutation($mybook_id: String!) {
+    mybook_delete(mybook_id: $mybook_id) {
+      status
+      msg
+      mybookcates {
+        _id
+        mybookcate_info {
+          name
+          seq
+        }
+      }
+      mybooks {
+        _id
+        mybook_info {
+          title
+          type
+          user_id
+          mybookcate_id
+          seq_in_category
+          hide_or_show
+          studylike
+          writelike
+          seq_in_studylike
+          seq_in_writelike
+        }
+      }
+    }
+  }
+`;
+
+export const BookUpdateMutation = gql`
+  mutation BookUpdateMutation($mybook_id: String!, $title: String, $hide_or_show: String) {
+    mybook_update(mybook_id: $mybook_id, title: $title, hide_or_show: $hide_or_show) {
+      status
+      msg
+      mybookcates {
+        _id
+        mybookcate_info {
+          name
+          seq
+        }
+      }
+      mybooks {
+        _id
+        mybook_info {
+          title
+          type
+          user_id
+          mybookcate_id
+          seq_in_category
+          hide_or_show
+          studylike
+          writelike
+          seq_in_studylike
+          seq_in_writelike
+        }
+      }
+    }
+  }
+`;
+
+export const PositioningBookMutation = gql`
+  mutation PositioningBookMutation($direction: String, $mybook_id: String!) {
+    mybook_changeorder(direction: $direction, mybook_id: $mybook_id) {
+      status
+      msg
+      mybookcates {
+        _id
+        mybookcate_info {
+          name
+          seq
+        }
+      }
+      mybooks {
+        _id
+        mybook_info {
+          title
+          type
+          user_id
+          mybookcate_id
+          seq_in_category
+          hide_or_show
+          studylike
+          writelike
+          seq_in_studylike
+          seq_in_writelike
+        }
+      }
+    }
+  }
+`;
--- pages/books/write/index.js
+++ pages/books/write/index.js
@@ -10,102 +10,8 @@ import { logIn, logOut } from "../../../redux/actions";
 import Router from "next/router";
 import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useMutation } from "@apollo/client";
 import CategorySettingModal from "../../../components/books/write/category/CategorySettingModal";
+import { GetCategory, BookDeleteMutation, BookUpdateMutation, PositioningBookMutation } from "../../../graphql/query/writemain";
 
-const GetCategory = gql`
-  query {
-    mybookcate_get {
-      status
-      msg
-      mybookcates {
-        _id
-        mybookcate_info {
-          user_id
-          name
-          seq
-        }
-      }
-    }
-    mybook_get {
-      status
-      msg
-      mybooks {
-        _id
-        mybook_info {
-          title
-          type
-          user_id
-          mybookcate_id
-          seq_in_category
-          hide_or_show
-          studylike
-          writelike
-          seq_in_studylike
-          seq_in_writelike
-        }
-      }
-    }
-  }
-`;
-const BookDeleteMutation = gql`
-  mutation BookDeleteMutation($mybook_id: String!) {
-    mybook_delete(mybook_id: $mybook_id) {
-      status
-      msg
-      mybookcates {
-        _id
-        mybookcate_info {
-          name
-          seq
-        }
-      }
-      mybooks {
-        _id
-        mybook_info {
-          title
-          type
-          user_id
-          mybookcate_id
-          seq_in_category
-          hide_or_show
-          studylike
-          writelike
-          seq_in_studylike
-          seq_in_writelike
-        }
-      }
-    }
-  }
-`;
-const BookUpdateMutation = gql`
-  mutation BookUpdateMutation($mybook_id: String!, $title: String, $hide_or_show: String) {
-    mybook_update(mybook_id: $mybook_id, title: $title, hide_or_show: $hide_or_show) {
-      status
-      msg
-      mybookcates {
-        _id
-        mybookcate_info {
-          name
-          seq
-        }
-      }
-      mybooks {
-        _id
-        mybook_info {
-          title
-          type
-          user_id
-          mybookcate_id
-          seq_in_category
-          hide_or_show
-          studylike
-          writelike
-          seq_in_studylike
-          seq_in_writelike
-        }
-      }
-    }
-  }
-`;
 const WriteComponent = () => {
   const dispatch = useDispatch();
   const { loading, error, data } = useQuery(GetCategory);
@@ -113,9 +19,33 @@ const WriteComponent = () => {
   const [books, setBooks] = useState();
   const [mybook_delete] = useMutation(BookDeleteMutation, { onCompleted: showdatadelete });
   const [mybook_update] = useMutation(BookUpdateMutation, { onCompleted: showdataupdate });
-  // const [books, setBooks] = useState(data.bookcategory_get.mybook.bookinfo);
-  console.log(category);
+  const [mybook_changeorder] = useMutation(PositioningBookMutation, { onCompleted: showdatareposition });
 
+  //카테고리내에 책 순서 변경
+  function showdatareposition(data) {
+    console.log("data", data);
+    setBooks(data.mybook_changeorder.mybooks);
+  }
+
+  async function positionBooks(direction, id) {
+    try {
+      await mybook_changeorder({
+        variables: {
+          direction: direction,
+          mybook_id: id,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const onFinishPositionBook = (direction, id) => {
+    console.log(direction, id);
+    positionBooks(direction, id);
+  };
+
+  // 책이름 변경 뮤테이션
   function showdataupdate(data) {
     console.log("data", data);
     setBooks(data.mybook_update.mybooks);
@@ -139,6 +69,7 @@ const WriteComponent = () => {
     updateBookName(values.title, values.id, values.hide_or_show);
   };
 
+  // 책 삭제 뮤테이션
   function showdatadelete(item) {
     console.log("data", item);
     if (item.mybook_delete.msg === "책 삭제 성공적!") {
@@ -162,9 +93,8 @@ const WriteComponent = () => {
     }
   }
 
+  //useEffect
   useEffect(() => {
-    console.log("컴포넌트가 화면에 나타남");
-
     if (data) {
       console.log(data);
       setCategory(data.mybookcate_get.mybookcates);
@@ -174,52 +104,40 @@ const WriteComponent = () => {
     console.log(books);
   });
 
-  const linkStyle = {
-    color: "black",
-    padding: 10,
-    fontSize: "1rem",
-  };
-
+  // 카테고리 설정 모달 visible
   const [isModalVisible, setIsModalVisible] = useState(false);
-
   const showModal = () => {
     setIsModalVisible(true);
   };
-
   const handleOk = () => {
     setIsModalVisible(false);
   };
-
   const handleCancel = () => {
     setIsModalVisible(false);
   };
 
   return (
     <div>
-      <LikeBookList />
+      <LikeBookList category={category} books={books} deleteBook={deleteBook} onFinishUpdate={onFinishUpdate} onFinishPositionBook={onFinishPositionBook} />
       <button onClick={showModal}>카테고리 설정</button>
       <CategorySettingModal isModalVisible={isModalVisible} handleOk={handleOk} handleCancel={handleCancel} />
       <Link href="/books/write/createbook">
-        <a style={linkStyle}>
+        <a>
           <button>새책만들기</button>
         </a>
       </Link>
-      <BookList category={category} books={books} deleteBook={deleteBook} onFinishUpdate={onFinishUpdate} />
+      <BookList category={category} books={books} deleteBook={deleteBook} onFinishUpdate={onFinishUpdate} onFinishPositionBook={onFinishPositionBook} />
     </div>
   );
 };
 
-const LikeBookList = () => {
-  return <div>like book list</div>;
-};
-
-const BookList = ({ category, books, deleteBook, onFinishUpdate }) => {
+//책 리스트 컨테이너
+const BookList = ({ category, books, deleteBook, onFinishUpdate, onFinishPositionBook }) => {
   if (category) {
     var categoryList = category.map((category) => (
       <Fragment key={category._id}>
-        <div>{category.mybookcate_info.name}</div>
         <div>
-          <BooksInCategory categoryId={category._id} books={books} deleteBook={deleteBook} onFinishUpdate={onFinishUpdate} />
+          <BooksInCategory cateName={category.mybookcate_info.name} categoryId={category._id} books={books} deleteBook={deleteBook} onFinishUpdate={onFinishUpdate} onFinishPositionBook={onFinishPositionBook} />
         </div>
       </Fragment>
     ));
@@ -244,7 +162,29 @@ const BookList = ({ category, books, deleteBook, onFinishUpdate }) => {
   );
 };
 
-const BooksInCategory = ({ books, categoryId, deleteBook, onFinishUpdate }) => {
+//카테고리내에 책 리스트 컴퍼넌트
+const BooksInCategory = ({ cateName, books, categoryId, deleteBook, onFinishUpdate, onFinishPositionBook }) => {
+  if (books) {
+    console.log(books);
+    const seq = books.filter((book) => book.mybook_info.mybookcate_id === categoryId);
+    const lastSeq = seq.length - 1;
+    if (lastSeq === 0) {
+      var totalLength = 0;
+    }
+    var list = books.map((book) => {
+      if (book.mybook_info.mybookcate_id === categoryId) {
+        return (
+          <Fragment key={book._id}>
+            <ListItem cateName={cateName} totalLength={totalLength} lastSeq={lastSeq} book={book} deleteBook={deleteBook} onFinishUpdate={onFinishUpdate} onFinishPositionBook={onFinishPositionBook} />
+          </Fragment>
+        );
+      }
+    });
+  }
+  return <>{list}</>;
+};
+
+const ListItem = ({ book, deleteBook, onFinishUpdate, onFinishPositionBook, lastSeq, totalLength,cateName }) => {
   const [updatenewInput, setupdateNewInput] = useState(false);
   const updateNameText = <span style={{ fontSize: "11px" }}>변경할 이름을 입력해 주세요.</span>;
   const updatecontent = (id, hide_or_show) => (
@@ -279,38 +219,100 @@ const BooksInCategory = ({ books, categoryId, deleteBook, onFinishUpdate }) => {
       </Space>
     </Form>
   );
+  return (
+    <>
+      <ul style={{ display: "flex", justifyContent: "space-between", listStyle: "none" }}>
+        {book.mybook_info.seq_in_category === 0 && <li>{cateName}</li>}
+        {book.mybook_info.seq_in_category !== 0 && <li style={{visibility:"hidden"}}>{cateName}</li>}
+        
+        <li>{book.mybook_info.title}</li>
+        <li>
+          <Popover placement="rightTop" title={updateNameText} visible={updatenewInput} content={updatecontent(book._id, book.mybook_info.hide_or_show)} trigger="click">
+            <button onClick={() => setupdateNewInput(true)} style={{ fontSize: "11px" }}>
+              이름바꾸기
+            </button>
+          </Popover>
+        </li>
+        <li>책정보 블라블라{lastSeq}</li>
+        <li>
+          {totalLength === 0 && (
+            <>
+              <button disabled>up</button>
+              <button disabled>down</button>
+            </>
+          )}
+          {totalLength !== 0 && book.mybook_info.seq_in_category === 0 && (
+            <>
+              <button disabled>up</button>
+              <button onClick={() => onFinishPositionBook("down", book._id)}>down</button>
+            </>
+          )}
+          {totalLength !== 0 && book.mybook_info.seq_in_category === lastSeq && (
+            <>
+              <button onClick={() => onFinishPositionBook("up", book._id)}>up</button>
+              <button disabled>down</button>
+            </>
+          )}
+        </li>
+        <li>
+          <button>좋아요</button>
+        </li>
+        <li>
+          <button>숨기기</button>
+        </li>
+        <li>
+          <button onClick={() => deleteBook(book._id)}>삭제</button>
+        </li>
+      </ul>
+    </>
+  );
+};
 
+
+const LikeBookList = ({ category, books, deleteBook, onFinishUpdate, onFinishPositionBook }) => {
+  if (category) {
+    var categoryList = category.map((category) => (
+      <Fragment key={category._id}>
+        <div>
+          <LikeBooksInCategory categoryName={category.mybookcate_info.name} categoryId={category._id} books={books} deleteBook={deleteBook} onFinishUpdate={onFinishUpdate} onFinishPositionBook={onFinishPositionBook} />
+        </div>
+      </Fragment>
+    ));
+  }
+
+  return (
+    <div>
+      <div>
+        <ul style={{ display: "flex", justifyContent: "space-between", listStyle: "none" }}>
+          <li>카테고리</li>
+          <li>책이름</li>
+          <li>이름바꾸기</li>
+          <li>책정보</li>
+          <li>순서변경</li>
+          <li>좋아요</li>
+          <li>숨김</li>
+          <li>삭제</li>
+        </ul>
+      </div>
+      {categoryList}
+    </div>
+  );
+};
+
+//카테고리내에 책 리스트 컴퍼넌트
+const LikeBooksInCategory = ({ categoryName, books, categoryId, deleteBook, onFinishUpdate, onFinishPositionBook }) => {
   if (books) {
-    console.log(books, "asdf");
-    console.log(categoryId);
+    console.log(books);
+    const seq = books.filter((book) => book.mybook_info.mybookcate_id === categoryId);
+    const lastSeq = seq.length - 1;
+    if (lastSeq === 0) {
+      var totalLength = 0;
+    }
     var list = books.map((book) => {
       if (book.mybook_info.mybookcate_id === categoryId) {
         return (
           <Fragment key={book._id}>
-            <ul style={{ display: "flex", justifyContent: "space-between", listStyle: "none" }}>
-              <li>{book.mybook_info.title}</li>
-              <li>
-                <Popover placement="rightTop" title={updateNameText} visible={updatenewInput} content={updatecontent(book._id, book.mybook_info.hide_or_show)} trigger="click">
-                  <button onClick={() => setupdateNewInput(true)} style={{ fontSize: "11px" }}>
-                    이름바꾸기
-                  </button>
-                </Popover>
-              </li>
-              <li>책정보 블라블라</li>
-              <li>
-                <button>up</button>
-                <button>down</button>
-              </li>
-              <li>
-                <button>좋아요</button>
-              </li>
-              <li>
-                <button>숨기기</button>
-              </li>
-              <li>
-                <button onClick={() => deleteBook(book._id)}>삭제</button>
-              </li>
-            </ul>
+            <LikeListItem categoryName={categoryName} totalLength={totalLength} lastSeq={lastSeq} book={book} deleteBook={deleteBook} onFinishUpdate={onFinishUpdate} onFinishPositionBook={onFinishPositionBook} />
           </Fragment>
         );
       }
@@ -318,6 +320,90 @@ const BooksInCategory = ({ books, categoryId, deleteBook, onFinishUpdate }) => {
   }
   return <>{list}</>;
 };
+
+const LikeListItem = ({ book, deleteBook, onFinishUpdate, onFinishPositionBook, lastSeq, totalLength, categoryName }) => {
+  const [updatenewInput, setupdateNewInput] = useState(false);
+  const updateNameText = <span style={{ fontSize: "11px" }}>변경할 이름을 입력해 주세요.</span>;
+  const updatecontent = (id, hide_or_show) => (
+    <Form
+      layout={"inline"}
+      size="small"
+      initialValues={{
+        id: id,
+        hide_or_show: hide_or_show,
+      }}
+      onFinish={onFinishUpdate}
+      className="change_book_title_input_form"
+    >
+      <Space>
+        <Form.Item name={["title"]} rules={[{ required: true }]}>
+          <Input placeholder="" />
+        </Form.Item>
+        <Form.Item name={["id"]} hidden={true} rules={[{ required: true }]}>
+          <Input placeholder={id} />
+        </Form.Item>
+        <Form.Item name={["hide_or_show"]} hidden={true} rules={[{ required: true }]}>
+          <Input placeholder={hide_or_show} />
+        </Form.Item>
+        <Form.Item className="change_book_title_buttons">
+          <Button type="primary" onClick={() => setupdateNewInput(false)} htmlType="submit">
+            완료
+          </Button>
+          <Button type="primary" onClick={() => setupdateNewInput(false)}>
+            취소
+          </Button>
+        </Form.Item>
+      </Space>
+    </Form>
+  );
+  return (
+    <>
+      <ul style={{ display: "flex", justifyContent: "space-between", listStyle: "none" }}>
+        <li>{categoryName}</li>
+        <li>{book.mybook_info.title}</li>
+        <li>
+          <Popover placement="rightTop" title={updateNameText} visible={updatenewInput} content={updatecontent(book._id, book.mybook_info.hide_or_show)} trigger="click">
+            <button onClick={() => setupdateNewInput(true)} style={{ fontSize: "11px" }}>
+              이름바꾸기
+            </button>
+          </Popover>
+        </li>
+        <li>책정보 블라블라{lastSeq}</li>
+        <li>
+          {totalLength === 0 && (
+            <>
+              <button disabled>up</button>
+              <button disabled>down</button>
+            </>
+          )}
+          {totalLength !== 0 && book.mybook_info.seq_in_category === 0 && (
+            <>
+              <button disabled>up</button>
+              <button onClick={() => onFinishPositionBook("down", book._id)}>down</button>
+            </>
+          )}
+          {totalLength !== 0 && book.mybook_info.seq_in_category === lastSeq && (
+            <>
+              <button onClick={() => onFinishPositionBook("up", book._id)}>up</button>
+              <button disabled>down</button>
+            </>
+          )}
+        </li>
+        <li>
+          <button>좋아요</button>
+        </li>
+        <li>
+          <button>숨기기</button>
+        </li>
+        <li>
+          <button onClick={() => deleteBook(book._id)}>삭제</button>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+
 const Write = () => {
   return (
     <Layout>

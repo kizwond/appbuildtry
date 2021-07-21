e46b78172919733b6c1f999d61d7f3ac12189baf
kizwond <kizwond@gmail.com>
2021-07-21 17:26:15 +0900

    ing

---
+++ pages/books/write/bookedit.js
@@ -0,0 +1,11 @@
+import Layout from "../../../components/layout/Layout";
+
+const BookEdit = () => {
+    return (
+        <Layout>
+            <div>edit</div>
+      </Layout>
+    )
+}
+
+export default BookEdit;

8f8f6fec4e1eac7e65dc4183a34b03cfa1629220
kizwond <kizwond@gmail.com>
2021-07-27 17:22:33 +0900

    index ing

--- pages/books/write/[bookid].js
+++ pages/books/write/[bookid].js
@@ -1,13 +1,17 @@
 import Link from "next/link";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Layout from "../../../components/layout/Layout";
 
 import LeftDrawer from "../../../components/books/write/editpage/LeftDrawer"
 import RightDrawer from "../../../components/books/write/editpage/RightDrawer"
+import { GetIndex, IndexCreateMutation } from "../../../graphql/query/writemain";
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useMutation } from "@apollo/client";
 
 const Book = () => {
   const { query } = useRouter();
   console.log(query);
+
   return (
     <Layout>
       <div style={{display:"flex", justifyContent:"space-between"}}>

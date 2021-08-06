3b183c613cd0c677f5a74ff57650aa61ed18f193
kizwond <kizwond@gmail.com>
2021-08-06 17:25:22 +0900

    refresh token related

--- graphql/query/account.js
+++ graphql/query/account.js
@@ -64,3 +64,24 @@ mutation {
     }
   }
 `;
+
+export const ResetToekn = gql`
+  mutation ResetToken($refreshToken: String) {
+    resetToken(refreshToken: $refreshToken) {
+      status
+      msg
+      users {
+        _id
+        user_info {
+          username
+          name
+          email
+        }
+      }
+      token {
+        accessToken
+        refreshToken
+      }
+    }
+  }
+`;

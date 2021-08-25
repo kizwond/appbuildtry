a39f47b4ada6ff2b91ad8d41eb5ed52c7c1ad3e5
kizwond <kizwond@gmail.com>
2021-08-25 16:12:19 +0900

    indextree

--- components/books/write/editpage/LeftDrawer.js
+++ components/books/write/editpage/LeftDrawer.js
@@ -3,6 +3,7 @@ import { Drawer, Button, Tree, Modal } from "antd";
 import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useMutation } from "@apollo/client";
 import { GetIndex, IndexCreateMutation, IndexRenameMutation, IndexLevelMutation, IndexDeleteMutation } from "../../../../graphql/query/bookIndex";
 import IndexSettingModal from "../index/IndexSettingModal";
+import { UnorderedListOutlined, DoubleLeftOutlined,CarryOutOutlined } from '@ant-design/icons';
 
 const LeftDrawer = ({ book_id }) => {
   const [visible, setVisible] = useState(false);
@@ -148,6 +149,331 @@ const LeftDrawer = ({ book_id }) => {
   const onSelect = (selectedKeys, info) => {
     console.log("selected", selectedKeys, info);
   };
+if(indexinfo){
+  console.log(indexinfo)
+  let level_all =[];
+  indexinfo.forEach((table, index)=>{
+        if(table){
+          if(table.level === 1){
+            let level = {
+              title: table.name,
+              index_id:table._id,
+             
+              level: 1,
+              icon: <CarryOutOutlined />,
+              children: [],}
+              level_all.push(level)
+          } else if(table.level === 2){
+            let level = {
+              title: table.name,
+              index_id:table._id,
+             
+              level: 2,
+              icon: <CarryOutOutlined />,
+              children: [],}
+              level_all.push(level)
+          } else if(table.level === 3){
+            let level = {
+              title: table.name,
+              index_id:table._id,
+             
+              level: 3,
+              icon: <CarryOutOutlined />,
+              children: [],}
+              level_all.push(level)
+          } else if(table.level === 4){
+            let level = {
+              title: table.name,
+              index_id:table._id,
+             
+              level: 4,
+              icon: <CarryOutOutlined />,
+              children: [],}
+              level_all.push(level)
+          } else if(table.level === 5){
+            let level = {
+              title: table.name,
+              index_id:table._id,
+             
+              level: 5,
+              icon: <CarryOutOutlined />,
+              children: [],}
+              level_all.push(level)
+          }     
+        } 
+      }
+    )
+  console.log(level_all)
+
+  
+const level_5 = obj => obj.level === 5;
+const level_4 = obj => obj.level === 4;
+const level_3 = obj => obj.level === 3;
+const level_2 = obj => obj.level === 2;
+if(level_all.length > 0){
+  let level_5_exist = level_all.some(level_5)
+  let level_4_exist = level_all.some(level_4)
+  let level_3_exist = level_all.some(level_3)
+  let level_2_exist = level_all.some(level_2)
+
+  //level_5 exist
+  if(level_5_exist === true){
+    let temp_data_4 = []
+
+    for(var i = 0; i < level_all.length; i += 1) {
+      if(level_all[i]['level'] === 4) {
+        temp_data_4.push(level_all[i])
+      } else if(level_all[i]['level'] === 5 && temp_data_4.length > 0) {
+        for(var a = 0; a < temp_data_4.length; a += 1) {
+          temp_data_4[temp_data_4.length - 1]['children'].push(level_all[i])
+          break;
+        }
+      } 
+    }
+     i = 0;
+    while (i < level_all.length) {
+      if (level_all[i]['level'] === 5) {
+        level_all.splice(i, 1);
+      } else {
+        ++i;
+      }
+    }
+
+    if(temp_data_4.length > 0){
+      let temp_data_3 = []
+      for( i = 0; i < level_all.length; i += 1) {
+        if(level_all[i]['level'] === 3) {
+          temp_data_3.push(level_all[i])
+        } else if(level_all[i]['level'] === 4) {
+          for( a = 0; a < temp_data_3.length; a += 1) {
+            temp_data_3[temp_data_3.length - 1]['children'].push(level_all[i])
+            break;
+          }
+        } 
+      }
+      
+      if(temp_data_3.length > 0){
+         i = 0;
+        while (i < level_all.length) {
+          if (level_all[i]['level'] === 4) {
+            level_all.splice(i, 1);
+          } else {
+            ++i;
+          }
+        }
+      }
+
+      let temp_data_2 = []
+      for( i = 0; i < level_all.length; i += 1) {
+        if(level_all[i]['level'] === 2) {
+          temp_data_2.push(level_all[i])
+        } else if(level_all[i]['level'] === 3) {
+          for( a = 0; a < temp_data_2.length; a += 1) {
+            temp_data_2[temp_data_2.length - 1]['children'].push(level_all[i])
+            break;
+          }
+        } 
+      }
+      
+      if(temp_data_2.length > 0){
+         i = 0;
+        while (i < level_all.length) {
+          if (level_all[i]['level'] === 3) {
+            level_all.splice(i, 1);
+          } else {
+            ++i;
+          }
+        }
+      }
+
+      let temp_data_1 = []
+      for( i = 0; i < level_all.length; i += 1) {
+        if(level_all[i]['level'] === 1) {
+          temp_data_1.push(level_all[i])
+        } else if(level_all[i]['level'] === 2) {
+          for( a = 0; a < temp_data_1.length; a += 1) {
+            temp_data_1[temp_data_1.length - 1]['children'].push(level_all[i])
+            break;
+          }
+        } 
+      }
+      
+      if(temp_data_1.length > 0){
+         i = 0;
+        while (i < level_all.length) {
+          if (level_all[i]['level'] === 2) {
+            level_all.splice(i, 1);
+          } else {
+            ++i;
+          }
+        }
+      }
+    }
+
+    console.log('result:',level_all)
+    //level_4 exist
+  } else if(level_4_exist === true){
+    let temp_data_3 = []
+      for( i = 0; i < level_all.length; i += 1) {
+        if(level_all[i]['level'] === 3) {
+          temp_data_3.push(level_all[i])
+        } else if(level_all[i]['level'] === 4) {
+          for( a = 0; a < temp_data_3.length; a += 1) {
+            temp_data_3[temp_data_3.length - 1]['children'].push(level_all[i])
+            break;
+          }
+        } 
+      }
+    
+    if(temp_data_3.length > 0){
+       i = 0;
+      while (i < level_all.length) {
+        if (level_all[i]['level'] === 4) {
+          level_all.splice(i, 1);
+        } else {
+          ++i;
+        }
+      }
+
+      let temp_data_2 = []
+      for( i = 0; i < level_all.length; i += 1) {
+        if(level_all[i]['level'] === 2) {
+          temp_data_2.push(level_all[i])
+        } else if(level_all[i]['level'] === 3) {
+          for( a = 0; a < temp_data_2.length; a += 1) {
+            temp_data_2[temp_data_2.length - 1]['children'].push(level_all[i])
+            break;
+          }
+        } 
+      }
+      
+      if(temp_data_2.length > 0){
+         i = 0;
+        while (i < level_all.length) {
+          if (level_all[i]['level'] === 3) {
+            level_all.splice(i, 1);
+          } else {
+            ++i;
+          }
+        }
+      }
+
+      let temp_data_1 = []
+      for( i = 0; i < level_all.length; i += 1) {
+        if(level_all[i]['level'] === 1) {
+          temp_data_1.push(level_all[i])
+        } else if(level_all[i]['level'] === 2) {
+          for( a = 0; a < temp_data_1.length; a += 1) {
+            temp_data_1[temp_data_1.length - 1]['children'].push(level_all[i])
+            break;
+          }
+        } 
+      }
+      
+      if(temp_data_1.length > 0){
+         i = 0;
+        while (i < level_all.length) {
+          if (level_all[i]['level'] === 2) {
+            level_all.splice(i, 1);
+          } else {
+            ++i;
+          }
+        }
+      }
+
+      
+
+    }console.log('result:',level_all)
+    //level_3 exist
+  } else if(level_3_exist === true){
+    let temp_data_2 = []
+      for( i = 0; i < level_all.length; i += 1) {
+        if(level_all[i]['level'] === 2) {
+          temp_data_2.push(level_all[i])
+        } else if(level_all[i]['level'] === 3) {
+          for( a = 0; a < temp_data_2.length; a += 1) {
+            temp_data_2[temp_data_2.length - 1]['children'].push(level_all[i])
+            break;
+          }
+        } 
+      }
+    
+    if(temp_data_2.length > 0){
+       i = 0;
+      while (i < level_all.length) {
+        if (level_all[i]['level'] === 3) {
+          level_all.splice(i, 1);
+        } else {
+          ++i;
+        }
+      }
+
+      let temp_data_1 = []
+      for( i = 0; i < level_all.length; i += 1) {
+        if(level_all[i]['level'] === 1) {
+          temp_data_1.push(level_all[i])
+        } else if(level_all[i]['level'] === 2) {
+          for( a = 0; a < temp_data_1.length; a += 1) {
+            temp_data_1[temp_data_1.length - 1]['children'].push(level_all[i])
+            break;
+          }
+        } 
+      }
+      
+      if(temp_data_1.length > 0){
+         i = 0;
+        while (i < level_all.length) {
+          if (level_all[i]['level'] === 2) {
+            level_all.splice(i, 1);
+          } else {
+            ++i;
+          }
+        }
+      }
+
+      
+
+    }console.log('result:',level_all)
+    //level_2 exist
+  } else if(level_2_exist === true){
+
+    let temp_data_1 = []
+    for( i = 0; i < level_all.length; i += 1) {
+      if(level_all[i]['level'] === 1) {
+        temp_data_1.push(level_all[i])
+      } else if(level_all[i]['level'] === 2) {
+        for( a = 0; a < temp_data_1.length; a += 1) {
+          temp_data_1[temp_data_1.length - 1]['children'].push(level_all[i])
+          break;
+        }
+      } 
+    }
+
+    if(temp_data_1.length > 0){
+         i = 0;
+        while (i < level_all.length) {
+          if (level_all[i]['level'] === 2) {
+            level_all.splice(i, 1);
+          } else {
+            ++i;
+          }
+        }
+
+
+    }
+  }
+}
+
+
+if(level_all.length > 0){
+  var treeData = level_all
+}
+
+}
+  
+
+
+
 
   return (
     <>

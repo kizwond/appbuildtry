32ddada93f0e024b9f30682dfa560c571446401a
MrJooka <mr.jooka@gmail.com>
2021-08-27 19:38:29 +0900

    학습 세션 설정 페이지 코딩 시작

--- pages/books/study/sessionSetting/index.js
+++ pages/books/study/sessionSetting/index.js
@@ -1,12 +1,68 @@
-import Layout from "../../../../components/layout/Layout";
-import Footer from "../../../../components/index/Footer";
+import React, { useEffect, useState, useRef } from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_SESSTION_CARDS_DATA_IN_INDEXES_BY_SELECTED_BOOKS_ID } from '../../../../graphql/query/studySessionSetting';
+import Layout from '../../../../components/layout/Layout';
+import Footer from '../../../../components/index/Footer';
 
-const sessionSetting = () => {
+const SessionSetting = () => {
+  const [cardsList, setCardsList] = useState([]);
+  const counter = useRef(0);
+  const bookIdsList = useRef([]);
+  const [indexesList, setIndexesList] = useState([]);
+  useEffect(() => {
+    const booklist = JSON.parse(sessionStorage.getItem('books_selected'));
+    console.log('북아이디리스트 설정 - 유즈 이펙트');
+    bookIdsList.current = booklist.map((book) => book.book_id);
+  }, [bookIdsList]);
+
+  const { loading, error, data, variables } = useQuery(
+    GET_SESSTION_CARDS_DATA_IN_INDEXES_BY_SELECTED_BOOKS_ID,
+    {
+      variables: { mybook_id: bookIdsList.current[counter.current] },
+      onCompleted: (received_data) => {
+        console.log('통신완료 후 onCompleted 코드 시작');
+        if (counter.current < bookIdsList.current.length - 1) {
+          console.log('카운터설정');
+          counter.current += 1;
+        }
+
+        console.log('카테고리설정');
+        setCardsList([...cardsList, received_data]);
+        console.log(received_data);
+        console.log('통신완료 후 onCopleted 코드 종료');
+      },
+    }
+  );
+
+  if (error) {
+    console.log('에러', error);
+    console.log(variables);
+    return <div>에러발생</div>;
+  }
+
+  console.log(bookIdsList.current[counter.current]);
+  console.log(cardsList);
   return (
     <Layout>
-      <div>세션설정</div>
+      <div>
+        {'책아이디 아이디 : ' + bookIdsList.current[counter.current]}
+        {/* {cardsList.length > 0
+          ? cardsList[0].session_getNumCardsbyIndex?.indexsets[0]?.indexset_info
+              .mybook_id
+          : null} */}
+      </div>
+      <div
+        style={{
+          marginLeft: '30px',
+          marginRight: '30px',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        {JSON.stringify(cardsList)}
+      </div>
       <Footer />
     </Layout>
   );
 };
-export default sessionSetting;
+export default SessionSetting;
--- pages/test.js
+++ pages/test.js
@@ -1,31 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import { useQuery } from '@apollo/client';
+import { GET_My_BOOK_CATEGORY_AND_MY_BOOK_CATEGORIES } from '../graphql/query/studyCategory';
 
 const Test = () => {
-  const [selectedCategory, setSelectedCategory] = useState('');
-  const [counter, setCounter] = useState(1);
+  const [selectedCategory, setSelectedCategory] = useState([]);
+  const counter = useRef(0);
+  const bookIdsList = useRef([]);
+  const [indexesList, setIndexesList] = useState([]);
+
   const { loading, error, data, refetch } = useQuery(
     GET_My_BOOK_CATEGORY_AND_MY_BOOK_CATEGORIES,
     {
-      variables: { mybook_id: book_id },
-      onCompleted: (_data) => {
-        setCategories(_data.mybookcate_get.mybookcates);
-        setSelectedCategory(
-          _data.mybook_getbyMybookid.mybooks[0].mybook_info.mybookcate_id
-        );
-        console.log('카테고리 정보 불러옴', _data);
+      variables: { mybook_id: bookIdsList.current[counter.current] },
+      onCompleted: (received_data) => {
+        console.log('통신완료 후 onCompleted 코드 시작');
+        if (counter.current < bookIdsList.current.length - 1) {
+          console.log('카운터설정');
+          counter.current += 1;
+        }
+
+        console.log('카테고리설정');
+        setSelectedCategory([
+          ...selectedCategory,
+          received_data.mybook_getbyMybookid.mybooks[0].mybook_info
+            .mybookcate_id,
+        ]);
+        console.log('통신완료 후 onCopleted 코드 종료');
       },
     }
   );
 
   useEffect(() => {
-    setSelectedCategory(
-      data.mybook_getbyMybookid.mybooks[0].mybook_info.mybookcate_id
-    );
-    setCounter((prev) => prev + 1);
-  }, [data]);
+    const booklist = JSON.parse(sessionStorage.getItem('books_selected'));
+    bookIdsList.current = booklist.map((book) => book.book_id);
+  }, [bookIdsList]);
 
-  return <div>{data && selectedCategory}</div>;
+  console.log(counter.current);
+
+  return (
+    <div>
+      {bookIdsList.current[counter.current]} 그리고 카테고리 아이디:{' '}
+      {selectedCategory.length > 0 &&
+        selectedCategory.map((i) => {
+          console.log('랜더링', i);
+          return i;
+        })[0]}
+    </div>
+  );
 };
 
 export default Test;

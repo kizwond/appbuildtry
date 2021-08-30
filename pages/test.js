375a33d9f2a588b097ddfb90c5347e19238da331
MrJooka <mr.jooka@gmail.com>
2021-08-30 17:14:39 +0900

    목차 treeData 코드 완료

--- pages/test.js
+++ pages/test.js
@@ -1,54 +1,99 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
-import { GET_My_BOOK_CATEGORY_AND_MY_BOOK_CATEGORIES } from '../graphql/query/studyCategory';
+import { GET_SESSTION_CARDS_DATA_IN_INDEXES_BY_SELECTED_BOOKS_ID } from '../graphql/query/studySessionSetting';
+import { Tree } from 'antd';
+import produce from 'immer';
 
 const Test = () => {
-  const [selectedCategory, setSelectedCategory] = useState([]);
-  const counter = useRef(0);
-  const bookIdsList = useRef([]);
-  const [indexesList, setIndexesList] = useState([]);
-
-  const { loading, error, data, refetch } = useQuery(
-    GET_My_BOOK_CATEGORY_AND_MY_BOOK_CATEGORIES,
+  const [treeData, setTreeData] = useState([]);
+  const { loading, error, data } = useQuery(
+    GET_SESSTION_CARDS_DATA_IN_INDEXES_BY_SELECTED_BOOKS_ID,
     {
-      variables: { mybook_id: bookIdsList.current[counter.current] },
-      onCompleted: (received_data) => {
-        console.log('통신완료 후 onCompleted 코드 시작');
-        if (counter.current < bookIdsList.current.length - 1) {
-          console.log('카운터설정');
-          counter.current += 1;
-        }
-
-        console.log('카테고리설정');
-        setTimeout(() => {
-          console.log(counter.current);
-          setSelectedCategory([
-            ...selectedCategory,
-            received_data.mybook_getbyMybookid.mybooks[0].mybook_info
-              .mybookcate_id,
-          ]);
-        }, 1000);
-        console.log('통신완료 후 onCopleted 코드 종료');
-      },
+      variables: { mybook_id: '612842d0914b6216d4733866' },
     }
   );
 
-  useEffect(() => {
-    const booklist = JSON.parse(sessionStorage.getItem('books_selected'));
-    bookIdsList.current = booklist.map((book) => book.book_id);
-  }, [bookIdsList]);
+  if (loading) <div>loading...</div>;
+  if (error) <div>loading...</div>;
 
-  // console.log(counter.current);
+  useEffect(() => {
+    if (data) {
+      const rawIndexes =
+        data.session_getNumCardsbyIndex.indexsets[0].indexes.map(
+          (item, index) => ({
+            title: item.name,
+            key: item._id,
+            level: item.level,
+            index: index,
+            children: [],
+          })
+        );
+      let data_for_tree_level_one = rawIndexes.filter(
+        (item) => item.level == 1
+      );
+      let data_for_tree_level_two = rawIndexes.filter(
+        (item) => item.level == 2
+      );
+      let data_for_tree_level_three = rawIndexes.filter(
+        (item) => item.level == 3
+      );
+      let data_for_tree_level_four = rawIndexes.filter(
+        (item) => item.level == 4
+      );
+      let data_for_tree_level_five = rawIndexes.filter(
+        (item) => item.level == 5
+      );
+
+      const generatorForChildrens = (parents, son) => {
+        for (let i = 0; i < parents.length; i++) {
+          for (let k = 0; k < son.length; k++) {
+            if (parents[i].index < son[k].index) {
+              if (i + 1 == parents.length) {
+                parents[i].children = [...parents[i].children, son[k]];
+              } else if (parents[i + 1].index > son[k].index) {
+                parents[i].children = [...parents[i].children, son[k]];
+              }
+            }
+          }
+        }
+      };
+
+      const getIndexes = async () => {
+        await generatorForChildrens(
+          data_for_tree_level_four,
+          data_for_tree_level_five
+        );
+        await generatorForChildrens(
+          data_for_tree_level_three,
+          data_for_tree_level_four
+        );
+        await generatorForChildrens(
+          data_for_tree_level_two,
+          data_for_tree_level_three
+        );
+        await generatorForChildrens(
+          data_for_tree_level_one,
+          data_for_tree_level_two
+        );
+        setTreeData(data_for_tree_level_one);
+      };
+
+      getIndexes();
+    }
+  }, [data]);
+
+  console.log(treeData);
+
+  const onCheck = (checkedKeysValue) => {
+    console.log('onCheck', checkedKeysValue);
+  };
 
   return (
-    <div>
-      {bookIdsList.current[counter.current]} 그리고 카테고리 아이디:{' '}
-      {selectedCategory.length > 0 &&
-        selectedCategory.map((i) => {
-          console.log('랜더링', i);
-          return i;
-        })[0]}
-    </div>
+    <>
+      {treeData.length > 0 ? (
+        <Tree checkable checkStrictly onCheck={onCheck} treeData={treeData} />
+      ) : null}
+    </>
   );
 };
 

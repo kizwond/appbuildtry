b65c1dba55959a67df97df026c63d6a620e6bbe0
kizwond <kizwond@gmail.com>
2021-09-14 13:02:03 +0900

    ing

--- next.config.js
+++ next.config.js
@@ -6,9 +6,9 @@ module.exports = {
   async rewrites() {
     return [
       {
-        destination: 'http://localhost:5000/:path*',
-        source: '/:path*',
+        destination: "http://localhost:5000/api/:path*",
+        source: "/api/:path*",
       },
-  ];
+    ];
   },
-}
+};

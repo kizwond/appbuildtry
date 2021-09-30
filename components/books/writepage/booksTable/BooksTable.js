af77751229aa0d60a9558179ce71d5ef45103b47
MrJooka <mr.jooka@gmail.com>
2021-09-30 12:15:46 +0900

    styledComponent css booksTable로 옮김

--- components/books/writepage/booksTable/BooksTable.js
+++ components/books/writepage/booksTable/BooksTable.js
@@ -8,6 +8,7 @@ import MoveToBookSetting from './MoveToBookSetting';
 import FavoriteBook from './FavoriteBook';
 import { VerticalAlignBottomOutlined, BarChartOutlined, DollarCircleFilled, DoubleLeftOutlined, DoubleRightOutlined, PauseOutlined } from '@ant-design/icons';
 import moment from '../../../../node_modules/moment/moment';
+import styled from 'styled-components';
 
 const BooksTable = ({ category, myBook, handleToGetMyBook, isPopupSomething, chagePopup }) => {
   const [expandedRowKeys, setExpandedRowKeys] = useState([]);
@@ -518,7 +519,7 @@ const BooksTable = ({ category, myBook, handleToGetMyBook, isPopupSomething, cha
   ];
 
   return (
-    <Card bordered={false} size="small">
+    <StyledCard bordered={false} size="small">
       <Table
         dataSource={dataSource}
         tableLayout="fixed"
@@ -567,8 +568,391 @@ const BooksTable = ({ category, myBook, handleToGetMyBook, isPopupSomething, cha
           },
         }}
       />
-    </Card>
+    </StyledCard>
   );
 };
 
 export default BooksTable;
+
+const StyledCard = styled(Card)`
+  /* 모든 폰트 사이즈 */
+  & * {
+    font-size: 0.8rem;
+  }
+
+  /* 카테고리 펼치기 아이콘 오른쪽 마진 조절 */
+  & .ant-table-row-indent + .ant-table-row-expand-icon {
+    margin-right: 2px;
+  }
+
+  /* 개별 책 펼치기  */
+  & .ant-drawer-content {
+    overflow: hidden;
+    background-color: #6c757d;
+    background-clip: padding-box;
+    border: 0;
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+  }
+  & .customCircleButton:hover {
+    background-color: #495057;
+  }
+  & .PushCustomCircleButton {
+    background-color: #212529;
+  }
+  & .PushCustomCircleButton:hover {
+    background-color: #a9a9a9;
+  }
+  & .PullCustomCircleButton:hover {
+    background-color: #a9a9a9;
+  }
+
+  & .FirstBookCustom > .anticon-arrow-up > svg,
+  & .LastBookCustom > .anticon-arrow-down > svg {
+    color: #4d4d4d;
+  }
+
+  /* 아이콘 크기 및 색상 - 부모 div Hover시 동작 포함 */
+  & .anticon-double-right > svg {
+    font-size: 18px;
+    color: #a3a3a3;
+  }
+  & .PushCustomCircleButton:hover > .anticon-double-right > svg {
+    font-size: 18px;
+    color: #fff;
+  }
+
+  & .anticon-double-left > svg {
+    font-size: 18px;
+    color: #a3a3a3;
+  }
+  & .PullCustomCircleButton:hover > .anticon-double-left > svg {
+    color: #fff;
+  }
+
+  & .anticon-arrow-down > svg {
+    font-size: 16px;
+    color: #dee2e6;
+  }
+  & .customCircleButton:hover > .anticon-arrow-down > svg {
+    color: #fff;
+  }
+
+  & .anticon-arrow-up > svg {
+    font-size: 16px;
+    color: #dee2e6;
+  }
+  & .customCircleButton:hover > .anticon-arrow-up > svg {
+    color: #fff;
+  }
+
+  & .anticon-star > svg {
+    font-size: 16px;
+  }
+  & .customCircleButton:hover > .anticon-star.writeUnliked > svg {
+    color: #fff;
+  }
+  & .customCircleButton:hover > .anticon-star.writeLiked > svg {
+    color: #fcc725;
+  }
+
+  & .anticon-eye > svg {
+    font-size: 16px;
+    color: #dee2e6;
+  }
+  & .customCircleButton:hover > .anticon-eye > svg {
+    color: #fff;
+  }
+
+  & .anticon-eye-invisible > svg {
+    font-size: 16px;
+    color: #dee2e6;
+  }
+  & .customCircleButton:hover > .anticon-eye-invisible > svg {
+    color: #fff;
+  }
+
+  & .anticon-setting > svg {
+    font-size: 16px;
+    color: #a3a3a3;
+  }
+
+  & .ant-table.ant-table-small .ant-table-tbody > tr > td {
+    padding: 0;
+  }
+
+  & .ant-table table {
+    border-collapse: collapse;
+    background-color: white;
+    overflow: hidden;
+  }
+
+  & .categoryCol {
+    border-bottom: none;
+  }
+
+  & .Row-Last-One {
+    position: relative;
+    z-index: 3;
+    background-color: white;
+  }
+
+  & .foldedCategory > .Row-First-Left > div {
+    background: #e0e2f4;
+    border-radius: 8px;
+    margin: 3px 0px;
+
+    padding-left: 15px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    font-size: 0.7rem;
+    @media screen and (min-width: 577px) and (max-width: 768px) {
+      padding-left: 8px;
+    }
+    @media screen and (min-width: 100px) and (max-width: 576px) {
+      padding-left: 4px;
+    }
+  }
+  & .MiddleHiddenBar > .Row-First-Left > div,
+  & .LastHiddenBar > .Row-First-Left > div {
+    background: #e0e2f4;
+    border-radius: 8px;
+    margin: 3px 0px;
+    display: flex;
+    align-items: center;
+    padding-left: 15px;
+    height: 30px;
+    font-size: 0.7rem;
+    @media screen and (min-width: 577px) and (max-width: 768px) {
+      padding-left: 8px;
+    }
+    @media screen and (min-width: 100px) and (max-width: 576px) {
+      padding-left: 4px;
+    }
+  }
+  & .HandleOnOffShow > span {
+    font-size: 0.7rem;
+  }
+
+  & .NoBooksCategory > .Row-First-Left > div {
+    background: rgb(228, 224, 224);
+    border-radius: 8px;
+    margin: 3px 0px;
+
+    padding-left: 15px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    font-size: 0.7rem;
+    @media screen and (min-width: 577px) and (max-width: 768px) {
+      padding-left: 8px;
+    }
+    @media screen and (min-width: 100px) and (max-width: 576px) {
+      padding-left: 4px;
+    }
+  }
+
+  & .foldedCategory > .categoryCol,
+  & .NoBooksCategory > .categoryCol,
+  & .LastHiddenBar > .categoryCol,
+  & .lastEvenBook > .categoryCol,
+  & .lastOddBook > .categoryCol {
+    border-bottom: 0.5px solid #b3b2b2;
+  }
+
+  & .LastHiddenBar > .Row-First-Left,
+  & .NoBooksCategory > .Row-First-Left,
+  & .foldedCategory > .Row-First-Left {
+    border-bottom: 0.5px solid #b3b2b2;
+  }
+
+  & .lastEvenBook > .Row-First-Left,
+  & .lastEvenBook > .normal,
+  & .lastEvenBook > .Row-Last-One {
+    border-bottom: 0.5px solid #b3b2b2;
+  }
+
+  & .lastEvenBook > .normal > div,
+  & .lastEvenBook > .Row-Last-One > div {
+    background-color: #f5f5f5;
+    height: 34px;
+    margin-bottom: 3px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  & .lastEvenBook > .Row-First-Left > div {
+    background-color: #f5f5f5;
+    height: 34px;
+    margin-bottom: 3px;
+    display: flex;
+    align-items: center;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+    padding-left: 15px;
+    @media screen and (min-width: 577px) and (max-width: 768px) {
+      padding-left: 8px;
+    }
+    @media screen and (min-width: 100px) and (max-width: 576px) {
+      padding-left: 4px;
+    }
+  }
+
+  & .lastEvenBook > .normal > div.BookOrder {
+    color: #f5f5f5;
+  }
+
+  & .lastEvenBook > .Row-Last-One > div {
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+  }
+
+  & .lastOddBook > .Row-First-Left,
+  & .lastOddBook > .normal,
+  & .lastOddBook > .Row-Last-One {
+    border-bottom: 0.5px solid #b3b2b2;
+  }
+
+  & .lastOddBook > .normal > div,
+  & .lastOddBook > .Row-Last-One > div {
+    background-color: #fff;
+    height: 34px;
+    margin-bottom: 3px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  & .lastOddBook > .Row-First-Left > div {
+    background-color: #fff;
+    height: 34px;
+
+    margin-bottom: 3px;
+    display: flex;
+    align-items: center;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+    padding-left: 15px;
+    @media screen and (min-width: 577px) and (max-width: 768px) {
+      padding-left: 8px;
+    }
+    @media screen and (min-width: 100px) and (max-width: 576px) {
+      padding-left: 4px;
+    }
+  }
+
+  & .lastOddBook > .normal > div.BookOrder {
+    color: #fff;
+  }
+
+  & .lastOddBook > .Row-Last-One > div {
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+  }
+
+  & .EvenNumberRow > .normal > div,
+  & .EvenNumberRow > .Row-Last-One > div {
+    background-color: #f5f5f5;
+    height: 34px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  & .EvenNumberRow > .Row-First-Left > div {
+    background-color: #f5f5f5;
+    height: 34px;
+
+    display: flex;
+    align-items: center;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+    padding-left: 15px;
+    @media screen and (min-width: 577px) and (max-width: 768px) {
+      padding-left: 8px;
+    }
+    @media screen and (min-width: 100px) and (max-width: 576px) {
+      padding-left: 4px;
+    }
+  }
+  & .EvenNumberRow > .normal > div.BookOrder {
+    color: #f5f5f5;
+  }
+
+  & .EvenNumberRow > .Row-Last-One > div {
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+  }
+
+  & .OddNumberRow > .normal > div,
+  & .OddNumberRow > .Row-Last-One > div {
+    background-color: #fff;
+    height: 34px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  & .OddNumberRow > .Row-First-Left > div {
+    background-color: #fff;
+    height: 34px;
+
+    display: flex;
+    align-items: center;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+    padding-left: 15px;
+    @media screen and (min-width: 577px) and (max-width: 768px) {
+      padding-left: 8px;
+    }
+    @media screen and (min-width: 100px) and (max-width: 576px) {
+      padding-left: 4px;
+    }
+  }
+  & .OddNumberRow > .normal > div.BookOrder {
+    color: #fff;
+  }
+
+  & .OddNumberRow > .Row-Last-One > div {
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+  }
+
+  /* & .ant-table-thead .categoryCol::before {
+    display: none;
+  } */
+  & .ant-table-thead .categoryCol {
+    border-bottom: 1px solid #f0f0f0;
+  }
+
+  & .ant-table-tbody > tr > td {
+    border-bottom: none;
+  }
+
+  & .singleBar {
+    width: 18px;
+    margin-left: 3px;
+    margin-right: 3px;
+  }
+  & .graphBar {
+    position: relative;
+    height: 20px;
+    background: rgba(237, 238, 233, 0);
+  }
+
+  & .AchivedCard {
+    position: absolute;
+    bottom: 0;
+    width: 18px;
+    background: #c5c6c7;
+    display: flex;
+    justify-content: center;
+  }
+
+  & .CardCounter {
+    position: absolute;
+    font-size: 0.6rem;
+    bottom: 3px;
+    display: block;
+  }
+`;

76d6e0512ce99b078f224105f22d630a34e5f2ea
kizwond <kizwond@gmail.com>
2021-10-15 08:42:08 +0900

    ing

--- components/books/write/editpage/WriteContainer.js
+++ components/books/write/editpage/WriteContainer.js
@@ -22,10 +22,6 @@ const Mobile = ({ children }) => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
   return isMobile ? children : null;
 };
-// const Default = ({ children }) => {
-//   const isNotMobile = useMediaQuery({ minWidth: 768 });
-//   return isNotMobile ? children : null;
-// };
 
 const WriteContainer = ({ indexChanged, indexSetId, book_id }) => {
   const ISSERVER = typeof window === "undefined";

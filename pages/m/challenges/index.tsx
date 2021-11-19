df57c69362d06c4afe5be54eaa7c37a911ce982c
MrJooka <mr.jooka@gmail.com>
2021-11-19 14:25:12 +0900

    ING

--- pages/m/challenges/index.tsx
+++ pages/m/challenges/index.tsx
@@ -1,8 +1,11 @@
+import { useQuery } from "@apollo/client";
 import { FunctionComponent } from "react";
 
 interface ChallengesProps {}
 
 const Challenges: FunctionComponent<ChallengesProps> = () => {
+  const { data, error, loading } = useQuery();
+
   return (
     <div>
       <div>도전 출판</div>

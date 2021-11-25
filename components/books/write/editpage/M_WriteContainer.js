b93d674c4fc1fc700fe90f839a4e85d93b8c8b7e
kizwond <kizwond@gmail.com>
2021-11-25 14:58:36 +0900

    ing

--- components/books/write/editpage/M_WriteContainer.js
+++ components/books/write/editpage/M_WriteContainer.js
@@ -218,7 +218,7 @@ const WriteContainer = ({ indexChanged, indexSetId, book_id, Editor, EditorFromC
           style={{ border: "1px solid lightgrey", borderBottom: "0px", padding: "5px", display: "flex", justifyContent: "space-between", fontSize: "0.8rem", alignItems: "center" }}
         >
           <div>카드 템플릿 선택 : </div>
-          <Select size="small" defaultValue={selectedCardTypeOption} style={{ width: 200, fontSize: "0.75rem" }} onChange={handleChange}>
+          <Select size="small" defaultValue={cardtype_info.name} style={{ width: 200, fontSize: "0.75rem" }} onChange={handleChange}>
             <Option value="default" style={{ fontSize: "0.8rem", color: "black", fontWeight: "700" }} disabled>
               카드타입선택
             </Option>
@@ -395,23 +395,26 @@ const WriteContainer = ({ indexChanged, indexSetId, book_id, Editor, EditorFromC
     }
   }
 
-  function onClickCardAddChild(type, parentId) {
+  function onClickCardAddChild(from, parentId, typeName) {
     console.log(parentId);
     sessionStorage.setItem("parentId", parentId);
     setEditorOn("");
-    if (selectedCardType === undefined) {
-      console.log(1)
-      console.log(cardTypes[0].cardtype_info)
-      setSelectedCardType(cardTypes[0].cardtype_info);
-      cardTypeInfo(cardTypes[0].cardtype_info, "inCard", parentId);
-      sessionStorage.setItem("cardtype", cardTypes[0].cardtype_info.cardtype);
-    } else {
-      console.log(2)
-      const hello = cardTypes.filter((item) => item.cardtype_info.name === selectedCardType.name);
+    if(from === "child"){
+      const hello = cardTypes.filter((item) => item.cardtype_info.name === typeName);
       setSelectedCardType(hello[0].cardtype_info);
       sessionStorage.setItem("cardtype", hello[0].cardtype_info.cardtype);
+      sessionStorage.setItem("selectedCardTypeId", hello[0]._id);
+      cardTypeInfo(hello[0].cardtype_info, "inCard", parentId);
+    } else if(from === "general"){
+      const hello = cardTypes.filter((item) => item.cardtype_info.cardtype === "flip");
+      console.log(hello)
+      setSelectedCardType(hello[0].cardtype_info);
+      sessionStorage.setItem("cardtype", hello[0].cardtype_info.cardtype);
+      sessionStorage.setItem("selectedCardTypeId", hello[0]._id);
       cardTypeInfo(hello[0].cardtype_info, "inCard", parentId);
     }
+      
+    
   }
 
   if (cards) {
@@ -419,15 +422,15 @@ const WriteContainer = ({ indexChanged, indexSetId, book_id, Editor, EditorFromC
       // console.log("카드에 스타일 입히기 시작", cardTypeSets);
 
       const current_card_style = cardTypeSets[0].cardtypes.filter((item) => item._id === content.card_info.cardtype_id);
-      // console.log(current_card_style);
+      console.log(current_card_style);
 
       const face_style = current_card_style[0].face_style;
       const row_style = current_card_style[0].row_style;
       const row_font = current_card_style[0].row_font;
 
       // console.log(row_font);
-      console.log(content);
-      console.log(contentsList);
+      // console.log(content);
+      // console.log(contentsList);
       const show_contents = contentsList.map((content_value) => {
         if (content_value._id === content.content.mycontent_id || content_value._id === content.content.buycontent_id) {
           // console.log(content_value._id, content.content.buycontent_id);
@@ -436,8 +439,8 @@ const WriteContainer = ({ indexChanged, indexSetId, book_id, Editor, EditorFromC
           } else {
             borderLeft = "none";
           }
-          // console.log("해당카드 정보", content);
-          // console.log("해당카드 정보", content_value);
+          console.log("해당카드 정보", content);
+          console.log("해당컨텐츠 정보", content_value);
 
           const figure_shape = makerFlagStyle.figure_style.shape;
           const figure_size = makerFlagStyle.figure_style.size;
@@ -1115,7 +1118,7 @@ const WriteContainer = ({ indexChanged, indexSetId, book_id, Editor, EditorFromC
                             <div>
                               <Button
                                 size="small"
-                                onClick={() => onClickCardAddChild("general", content.card_info.parentCard_id)}
+                                onClick={() => onClickCardAddChild("child", content.card_info.parentCard_id, current_card_style[0].cardtype_info.name)}
                                 style={{ fontSize: "0.75rem", border: "1px solid grey" }}
                               >
                                 자식카드추가
@@ -1299,7 +1302,7 @@ const WriteContainer = ({ indexChanged, indexSetId, book_id, Editor, EditorFromC
                               <div>
                                 <Button
                                   size="small"
-                                  onClick={() => onClickCardAddChild("general", content.card_info.parentCard_id)}
+                                  onClick={() => onClickCardAddChild("child", content.card_info.parentCard_id, current_card_style[0].cardtype_info.name)}
                                   style={{ fontSize: "0.75rem", border: "1px solid grey" }}
                                 >
                                   자식카드추가

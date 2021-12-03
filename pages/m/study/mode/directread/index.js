428790b5b4c2bc6a93d994118e09e3bafde5575f
kizwond <kizwond@gmail.com>
2021-12-03 17:24:18 +0900

    ing

--- pages/m/study/mode/directread/index.js
+++ pages/m/study/mode/directread/index.js
@@ -7,12 +7,43 @@ const FroalaEditorView = dynamic(() => import("react-froala-wysiwyg/FroalaEditor
     ssr: false,
   });
 
+
 const SessionSetting = () => {
+
+    // const []
+    // const hide = () => {
+    //     const textSelected = getSelectionText();
+    //     console.log(textSelected)
+    
+    //     var one = document.getElementById('temp');
+    //     var sibling = one.previousSibling;
+    //     var stringy = sibling.outerHTML
+    //     console.log(sibling)
+    //     console.log(stringy)
+    //     const change = stringy.replace(textSelected, `<span style={{display:"none"}}>${textSelected}</span>`)
+    //     console.log(change)
+    //     var nodes = document.fromString(change);
+    // }
+    
+    // function getSelectionText() {
+    //     var text = "";
+    //     if (window.getSelection) {
+    //         text = window.getSelection().toString();
+    //     } else if (document.selection && document.selection.type != "Control") {
+    //         text = document.selection.createRange().text;
+    //     }
+    //     return text;
+    // }
+
   return (
     <>
       <StudyLayout>
         <div style={{ width: "90%", margin: "auto", marginBottom: "120px", marginTop: "50px" }}>
             {/* <DirectReadContainer FroalaEditorView={FroalaEditorView}/> */}
+            {/* <div>hello there</div>
+                {nodes}
+            <button id="temp" onClick={hide}>버튼</button> */}
+            hello
         </div>
       </StudyLayout>
     </>

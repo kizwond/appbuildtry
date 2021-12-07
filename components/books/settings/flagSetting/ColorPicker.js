00f7d8a4739dff05c8076ea06acd225e6366d06e
MrJooka <mr.jooka@gmail.com>
2021-12-07 17:19:01 +0900

    ing

--- components/books/settings/flagSetting/ColorPicker.js
+++ components/books/settings/flagSetting/ColorPicker.js
@@ -1,6 +1,7 @@
-import React, { memo, useState } from 'react';
-import reactCSS from 'reactcss';
-import { GithubPicker } from 'react-color';
+import React, { memo, useState } from "react";
+import reactCSS from "reactcss";
+import { CompactPicker, GithubPicker } from "react-color";
+import { Button } from "antd";
 
 const ColorPicker = memo(({ color, onChangeColor, index }) => {
   const [displayColorPicker, setDisplayColorPicker] = useState(false);
@@ -21,43 +22,46 @@ const ColorPicker = memo(({ color, onChangeColor, index }) => {
   const styles = reactCSS({
     default: {
       color: {
-        width: '36px',
-        height: '14px',
-        borderRadius: '2px',
+        width: "50px",
+        // height: "14px",
+        borderRadius: "2px",
         background: color,
       },
       swatch: {
-        padding: '5px',
-        background: '#fff',
-        borderRadius: '1px',
-        boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
-        display: 'inline-block',
-        cursor: 'pointer',
+        // padding: "5px",
+        background: "#fff",
+        borderRadius: "1px",
+        boxShadow: "0 0 0 1px rgba(0,0,0,.1)",
+        display: "inline-block",
+        cursor: "pointer",
       },
       popover: {
-        position: 'absolute',
-        zIndex: '2',
+        position: "absolute",
+        zIndex: "2",
       },
       cover: {
-        position: 'fixed',
-        top: '0px',
-        right: '0px',
-        bottom: '0px',
-        left: '0px',
+        position: "fixed",
+        top: "0px",
+        right: "0px",
+        bottom: "0px",
+        left: "0px",
       },
     },
   });
   return (
     <div>
-      <div style={styles.swatch} onClick={handleClick}>
-        <div style={styles.color} />
-      </div>
+      {/* <div style={styles.swatch} onClick={handleClick}> */}
+      <Button style={styles.color} onClick={handleClick}>
+        {" "}
+      </Button>
+      {/* </div> */}
       {displayColorPicker ? (
         <div style={styles.popover}>
           <div style={styles.cover} onClick={handleClose} />
-          <GithubPicker
+          <CompactPicker
             color={color}
-            colors={['blue', 'yellow', 'green', 'black', 'pink']}
+            // 기본색 설정 부분
+            // colors={['blue', 'yellow', 'green', 'black', 'pink']}
             onChange={handleChange}
           />
         </div>

7f4b8477051fca54962ea8d91b56a111b50a503f
MrJooka <mr.jooka@gmail.com>
2021-12-09 18:06:12 +0900

    플래그 css 완

--- pages/test.js
+++ pages/test.js
@@ -1,39 +1,46 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFlag, faStar } from "@fortawesome/free-solid-svg-icons";
-import { Card, Space } from "antd";
+import React from "react";
+import UserFlagIcon from "../components/common/commonComponent/UserFlagIcon";
+import { Space } from "antd";
 
 const Test = () => {
   return (
-    <Card>
-      <Space size={30}>
-        <span className="fa-layers fa-fw" style={{ fontSize: "16px" }}>
-          <FontAwesomeIcon icon={faFlag} size="2x" />
-          <span
-            style={{
-              position: "absolute",
-              color: "white",
-              fontSize: "10px",
-              fontFamily: "NanumGothic",
-            }}
-          >
-            2
-          </span>
-        </span>
-        <span className="fa-layers fa-fw" style={{ fontSize: "16px" }}>
-          <FontAwesomeIcon icon={faStar} size="2x" color="red" />
-          <span
-            style={{
-              position: "absolute",
-              color: "white",
-              fontSize: "10px",
-              fontFamily: "NanumGothic",
-            }}
-          >
-            5
-          </span>
-        </span>
-      </Space>
-    </Card>
+    <Space direction="vertical">
+      <UserFlagIcon
+        figure="bookmark"
+        color="orange"
+        textColor="blue"
+        flagNumber="3"
+        iconRemSize="1"
+      />
+      <UserFlagIcon
+        figure="star"
+        color="orange"
+        textColor="blue"
+        flagNumber="5"
+        iconRemSize="1"
+      />
+      <UserFlagIcon
+        figure="heart"
+        color="orange"
+        textColor="blue"
+        flagNumber="6"
+        iconRemSize="1"
+      />
+      <UserFlagIcon
+        figure="tag"
+        color="orange"
+        textColor="blue"
+        flagNumber="2"
+        iconRemSize="1"
+      />
+      <UserFlagIcon
+        figure="flag"
+        color="orange"
+        textColor="blue"
+        flagNumber="2"
+        iconRemSize="1"
+      />
+    </Space>
   );
 };
 

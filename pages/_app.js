a49c63310c2c6e7ce9bcd658b7e8ebf097a15854
MrJooka <mr.jooka@gmail.com>
2021-12-10 18:21:48 +0900

    ing

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "antd/dist/antd.css";
 import "../styles/globals.css";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
+import "../node_modules/@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
 import wrapper from "../redux/store/configureStore";

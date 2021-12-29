4c8df7888b6b1f61d7b2265113c9358a766eb509
MrJooka <mr.jooka@gmail.com>
2021-12-29 17:50:38 +0900

    ing

--- graphql/fragment/cardSet.js
+++ graphql/fragment/cardSet.js
@@ -15,101 +15,65 @@ export const FRAGMENT_CARD_SET = gql`
         mybook_id
         indexset_id
         index_id
+        cardset_id
+        cardtypeset_id
+        cardtype_id
         cardtype
+        time_created
+        hasParent
+        parentCard_id
       }
       content {
         userFlag
         makerFlag {
           value
+          comment
         }
+        hidden {
+          _id
+          color
+          targetWord
+          toolType
+        }
+        underline {
+          _id
+          color
+          targetWord
+          toolType
+        }
+        highlight {
+          _id
+          color
+          targetWord
+          toolType
+        }
+        location
+        mycontent_id
+        buycontent_id
+        memo
       }
       studyStatus {
         statusCurrent
+        recentKnowTime
         recentStudyResult
         recentExamResult
         recentExamTime
         recentStudyTime
+        recentSelection
+        recentSelectTime
         needStudyTime
+        currentLevStudyTimes
+        currentLevElapsedTime
+        currentLevStudyHour
         totalStudyTimes
+        totalExamTimes
+        recentStudyHour
+        totalStayHour
         levelCurrent
       }
     }
   }
 `;
-// export const FRAGMENT_CARD_SET = gql`
-//   fragment MyCardSetFragment on Cardset {
-//     _id
-//     cardset_info {
-//       user_id
-//       mybook_id
-//       indexset_id
-//       index_id
-//     }
-//     cards {
-//       _id
-//       card_info {
-//         mybook_id
-//         indexset_id
-//         index_id
-//         cardset_id
-//         cardtypeset_id
-//         cardtype_id
-//         cardtype
-//         time_created
-//         hasParent
-//         parentCard_id
-//       }
-//       content {
-//         userFlag
-//         makerFlag {
-//           value
-//           comment
-//         }
-//         hidden {
-//           _id
-//           color
-//           targetWord
-//           toolType
-//         }
-//         underline {
-//           _id
-//           color
-//           targetWord
-//           toolType
-//         }
-//         highlight {
-//           _id
-//           color
-//           targetWord
-//           toolType
-//         }
-//         location
-//         mycontent_id
-//         buycontent_id
-//         memo
-//       }
-//       studyStatus {
-//         statusCurrent
-//         recentKnowTime
-//         recentStudyResult
-//         recentExamResult
-//         recentExamTime
-//         recentStudyTime
-//         recentSelection
-//         recentSelectTime
-//         needStudyTime
-//         currentLevStudyTimes
-//         currentLevElapsedTime
-//         currentLevStudyHour
-//         totalStudyTimes
-//         totalExamTimes
-//         recentStudyHour
-//         totalStayHour
-//         levelCurrent
-//       }
-//     }
-//   }
-// `;
 
 export const FRAGMENT_CARD_SET_WITHOUT_STUDY_STATUS = gql`
   fragment MyCardSetFragmentWithoutStudyStatus on Cardset {

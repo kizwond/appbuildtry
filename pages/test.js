fae303e10f4fb1c2a98dcb1571b96bdf150c82d5
MrJooka <mr.jooka@gmail.com>
2021-12-30 11:03:15 +0900

    ing

--- pages/test.js
+++ pages/test.js
@@ -1,7 +1,22 @@
+import { useRouter } from "next/router";
 import React from "react";
+import { QUERY_SESSION_CONFIG_AND_INDEXSET_AND_CARDSET_BY_BOOK_IDS } from "../graphql/query/allQuery";
 
 const test = () => {
   return <div>d</div>;
 };
 
 export default test;
+
+// export async function getStaticProps(context) {
+//   const { book_ids} = context.params
+//   const apolloClient = initializeApollo()
+//   await apolloClient.query({
+//     query: QUERY_SESSION_CONFIG_AND_INDEXSET_AND_CARDSET_BY_BOOK_IDS,
+//     variables: {mybook_ids: book_ids},
+//   })
+// initializeApollo
+//   return addApolloState(apolloClient, {
+//     props: {},
+//   })
+// }

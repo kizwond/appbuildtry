0560991e20fdbb7414226b38615128b11945ca4e
MrJooka <mr.jooka@gmail.com>
2022-01-03 11:15:17 +0900

    ing

--- components/common/styledComponent/antd/StyledAntdTabs.js
+++ components/common/styledComponent/antd/StyledAntdTabs.js
@@ -64,4 +64,24 @@ export const StyledAntTabs = styled(Tabs)`
     overflow-y: auto;
     height: ${({ isPc }) => (isPc ? "calc(100vh - 160px)" : "auto")};
   }
+
+  .ant-tabs-tabpane.ant-tabs-tabpane-active
+    > .SessionTabContentWrapper::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+    background-color: #f5f5f5;
+  }
+
+  .ant-tabs-tabpane.ant-tabs-tabpane-active
+    > .SessionTabContentWrapper::-webkit-scrollbar {
+    width: 6px;
+    background-color: #f5f5f5;
+  }
+
+  .ant-tabs-tabpane.ant-tabs-tabpane-active
+    > .SessionTabContentWrapper::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: #acacac;
+  }
 `;

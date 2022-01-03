1cf7fec9975f4fe665bb010aa1377def4ae950c9
MrJooka <mr.jooka@gmail.com>
2022-01-03 16:05:40 +0900

    ing

---
+++ pages/mybooks/settings/[book_id].js
@@ -0,0 +1,295 @@
+/* eslint-disable react/display-name */
+import React, { useState } from "react";
+
+import { useQuery } from "@apollo/client";
+import { QUERY_USER_BOOKS_BY_BOOK_IDS__WITH_USER_CATEGORIES } from "../../../graphql/query/allQuery";
+
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+import { Card, Select, Space, Avatar } from "antd";
+import {
+  AppstoreAddOutlined,
+  CloseOutlined,
+  DeleteOutlined,
+  FileSyncOutlined,
+  FlagOutlined,
+  FundViewOutlined,
+  RiseOutlined,
+} from "@ant-design/icons";
+
+import styled from "styled-components";
+import { StyledDivEllipsis } from "../../../components/common/styledComponent/page";
+
+import Layout from "../../../components/layout/Layout";
+import M_FlagSetting from "../../../components/books/settings/flagSetting/M_FlagSetting";
+import M_DeleteBook from "../../../components/books/settings/deleteBook/M_DeleteBook";
+import M_LevelAndCycleSetting from "../../../components/books/settings/levelAndCycleSetting/M_LevelAndCycleSetting";
+import M_ReAssignBookToAnotherCategory from "../../../components/books/settings/reasignBookToAnotherCategory/M_ReAssignBookToAnotherCategory";
+
+const BookSetting = () => {
+  const { query, push, back } = useRouter();
+
+  const [selectedMenu, setSelectedMenu] = useState("");
+
+  const { data, loading, error } = useQuery(
+    QUERY_USER_BOOKS_BY_BOOK_IDS__WITH_USER_CATEGORIES,
+    {
+      onCompleted: (_data) => {
+        if (
+          _data.mybookcateset_getMybookcatesetByUserID.status === "200" &&
+          _data.mybook_getMybookByMybookIDs.status === "200"
+        ) {
+          console.log("책 1권 정보 및 모든 책 카테 정보 받음", _data);
+        } else if (
+          _data.mybookcateset_getMybookcatesetByUserID.status === "401"
+        ) {
+          push("/m/account/login");
+        } else {
+          console.log("어떤 문제가 발생함");
+        }
+      },
+      variables: {
+        mybook_ids: [query.book_id],
+      },
+    }
+  );
+
+  if (loading) <div>로딩 중..</div>;
+  if (error) <div>에러 남</div>;
+
+  const content = (menu_item) => {
+    switch (menu_item) {
+      case "study_data_detail":
+        return null;
+        break;
+      case "set_flags":
+        return <M_FlagSetting />;
+        break;
+      case "cards_progress":
+        return null;
+        break;
+      case "set_level_and_cycle":
+        return <M_LevelAndCycleSetting book_id={query.book_id} />;
+        break;
+      case "select_category":
+        return (
+          <M_ReAssignBookToAnotherCategory
+            book_id={query.book_id}
+            cateIdNow={
+              data.mybook_getMybookByMybookIDs.mybooks[0].mybook_info
+                .mybookcate_id
+            }
+            categories={
+              data.mybookcateset_getMybookcatesetByUserID.mybookcatesets[0]
+                .mybookcates
+            }
+            bookTitle={
+              data.mybook_getMybookByMybookIDs.mybooks[0].mybook_info.title
+            }
+          />
+        );
+        break;
+      case "delete_book":
+        return <M_DeleteBook book_id={query.book_id} />;
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>콕북 - 책 상세 설정</title>
+        <meta charSet="utf-8" />
+      </Head>
+      {data &&
+        data.mybook_getMybookByMybookIDs.mybooks &&
+        data.mybook_getMybookByMybookIDs.mybooks.length === 1 && (
+          <>
+            <Layout>
+              <StyledMainPageCard
+                bordered={false}
+                size="small"
+                title={
+                  <>
+                    <StyledForHeader>
+                      <EllipsisTitle>
+                        책 제목:{" "}
+                        {
+                          data.mybook_getMybookByMybookIDs.mybooks[0]
+                            .mybook_info.title
+                        }
+                      </EllipsisTitle>
+                      <button
+                        type="button"
+                        aria-label="Close"
+                        className="ForCloseButton"
+                        onClick={() => {
+                          back();
+                        }}
+                      >
+                        <CloseOutlined />
+                      </button>
+                    </StyledForHeader>
+
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "flex-end",
+                        gap: "14px",
+                      }}
+                    >
+                      <div className="ForPageMainTitle">상세 설정</div>
+                    </div>
+                  </>
+                }
+              >
+                <div style={{ display: "flex" }}>
+                  <div
+                    className="BookSettingSelector"
+                    onChange={setSelectedMenu}
+                    optionLabelProp="label"
+                    placeholder="메뉴를 선택해주세요"
+                    listHeight={400}
+                    style={{ width: "240px", marginBottom: "8px" }}
+                  >
+                    <Select.Option
+                      value="study_data_detail"
+                      label="학습 상세 정보 보기"
+                    >
+                      <Space>
+                        <Avatar>
+                          <RiseOutlined />
+                        </Avatar>
+                        학습 상세 정보 보기
+                      </Space>
+                    </Select.Option>
+                    <Select.Option value="set_flags" label="플래그 설정">
+                      <Space>
+                        <Avatar>
+                          <FlagOutlined />
+                        </Avatar>
+                        플래그 설정
+                      </Space>
+                    </Select.Option>
+                    <Select.Option
+                      value="cards_progress"
+                      label="카드 학습 상태 관리"
+                    >
+                      <Space>
+                        <Avatar>
+                          <FundViewOutlined />
+                        </Avatar>
+                        카드 학습 상태 관리
+                      </Space>
+                    </Select.Option>
+                    <Select.Option
+                      value="set_level_and_cycle"
+                      label="레벨 및 복습 주기 설정"
+                    >
+                      <Space>
+                        <Avatar>
+                          <FileSyncOutlined />
+                        </Avatar>
+                        레벨 및 복습 주기 설정
+                      </Space>
+                    </Select.Option>
+                    <Select.Option
+                      value="select_category"
+                      label="카테고리 이동"
+                    >
+                      <Space>
+                        <Avatar>
+                          <AppstoreAddOutlined />
+                        </Avatar>
+                        카테고리 이동
+                      </Space>
+                    </Select.Option>
+                    <Select.Option value="delete_book" label="책 삭제">
+                      <Space>
+                        <Avatar>
+                          <DeleteOutlined />
+                        </Avatar>
+                        책 삭제
+                      </Space>
+                    </Select.Option>
+                  </div>
+                  <div>{content(selectedMenu)}</div>
+                </div>
+              </StyledMainPageCard>
+            </Layout>
+          </>
+        )}
+    </div>
+  );
+};
+export default BookSetting;
+
+const StyledMainPageCard = styled(Card)`
+  width: 1024px;
+  margin: 0 auto;
+  padding: 0 8px;
+
+  &.ant-card-small > .ant-card-head {
+    border-bottom: none;
+    padding: 0 8px;
+  }
+
+  &.ant-card-small > .ant-card-body {
+    padding: 0px 8px 12px 8px;
+    width: 100%;
+    & * {
+      font-size: 1rem;
+    }
+  }
+
+  & .fa-layers.fa-fw {
+  }
+
+  & .compact-picker {
+    width: 280px !important;
+  }
+
+  /* ColorPicker 그림자 설정용 서식 지움 */
+  & .ForSelectColorPicker + div > div:nth-child(2) > div:nth-child(1) {
+    display: none;
+  }
+  .ant-table table {
+    // ColorPicker 잘리는 문제 해결
+    overflow: unset;
+  }
+`;
+
+const StyledForHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 4px;
+  padding-bottom: 4px;
+  border-bottom: 1px dashed lightgray;
+  & .ForCloseButton {
+    display: inline-block;
+    color: rgba(0, 0, 0, 0.45);
+    font-weight: 700;
+    font-size: 16px;
+    font-style: normal;
+    line-height: 1;
+    text-align: center;
+    text-transform: none;
+    text-decoration: none;
+    background: transparent;
+    border: 0;
+    outline: 0;
+    cursor: pointer;
+    -webkit-transition: color 0.3s;
+    transition: color 0.3s;
+    text-rendering: auto;
+  }
+`;
+const EllipsisTitle = styled(StyledDivEllipsis)`
+  font-size: 1rem;
+  color: #747474;
+`;

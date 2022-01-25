2dc82a1403f8d907d274cba49c1ec8b9cc23941a
kizwond <kizwond@gmail.com>
2022-01-25 16:33:32 +0900

    exam mode examLog handling

--- components/books/study/mode/flip/ExamContainer.js
+++ components/books/study/mode/flip/ExamContainer.js
@@ -214,9 +214,9 @@ class Container extends Component {
     }
   };
 
-  saveAnswer = (current_card_info, answer) => {
-    console.log(current_card_info, answer);
-    const submitted = { cardId: current_card_info._id, answer: answer };
+  saveAnswer = (current_card_info, answer, content) => {
+    console.log(current_card_info, answer, content.face1[0]);
+    const submitted = { cardId: current_card_info._id, answer: answer, content:content.face1[0] };
     const examLog = JSON.parse(sessionStorage.getItem("examLog"));
     if (examLog === null) {
       sessionStorage.setItem("examLog", JSON.stringify([submitted]));
@@ -227,6 +227,7 @@ class Container extends Component {
         sessionStorage.setItem("examLog", JSON.stringify(logs));
       } else {
         examLog[exist].answer = answer;
+        examLog[exist].content = content.face1[0];
         sessionStorage.setItem("examLog", JSON.stringify(examLog));
       }
     }
@@ -264,6 +265,16 @@ class Container extends Component {
   };
 
   finishStudy = () => {
+    const examLog = JSON.parse(sessionStorage.getItem("examLog"))
+    console.log(examLog.filter(item=> item.answer !== ""))
+    const answerExist = examLog.filter(item=>item.answer !== "")
+    const lastIdOfExist = answerExist[answerExist.length-1].cardId
+    console.log(lastIdOfExist)
+    const indexOfLastExist = examLog.findIndex(item => item.cardId === lastIdOfExist)
+    console.log(indexOfLastExist)
+    examLog.splice(indexOfLastExist+1, examLog.length -1)
+    console.log(examLog)
+    sessionStorage.setItem("examLog", JSON.stringify(examLog))
     window.location.href = "/m/study/examresult";
   };
 
@@ -731,7 +742,7 @@ class Container extends Component {
                       content_value.selection.length > 0 &&
                       content_value.selection.map((item, index) => (
                         <>
-                          <Radio.Group defaultValue={answerField} buttonStyle="solid" size="small" onChange={(e) => this.saveAnswer(content, e.target.value)}>
+                          <Radio.Group defaultValue={answerField} buttonStyle="solid" size="small" onChange={(e) => this.saveAnswer(content, e.target.value,content_value)}>
                             {index === 0 && (
                               <>
                                 <Radio.Button value="1">➀</Radio.Button>
@@ -784,7 +795,7 @@ class Container extends Component {
                               <Input type="text" />
                             </>
                           )} */}
-                          <Input type="text" onChange={(e) => this.saveAnswer(content, e.target.value)} defaultValue={answerField} />
+                          <Input type="text" onChange={(e) => this.saveAnswer(content, e.target.value,content_value)} defaultValue={answerField} />
                         </>
                       ))}
                   </>

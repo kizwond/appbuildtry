9f1f02f17d142f75a49fb6018ce50fff226146c8
MrJooka <mr.jooka@gmail.com>
2022-01-26 10:48:56 +0900

    ing

--- pages/m/study/examresult/index.js
+++ pages/m/study/examresult/index.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useEffect } from "react";
 import M_Layout from "../../../../components/layout/M_Layout";
 
 import CardResultWrapper from "../../../../components/books/study/examResult/CardResultWrapper";
+import moment from "moment";
+import { useCallback } from "react";
+import prettyMilliseconds from "pretty-ms";
+import BoxForSummaryOfMainPage from "../../../../components/common/commonComponent/BoxForSummaryOfMainPage";
 const ExamResult = () => {
   const [isMounted, setIsMounted] = useState(false);
   useEffect(() => setIsMounted(true), []);
@@ -23,7 +27,8 @@ const ExamResult = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <M_Layout>
-        <div className="w-full mx-auto absolute top-[40px] h-[calc(100vh_-_40px)] overflow-y-auto px-[8px] min-w-[360px] pb-[15px] pt-[8px]">
+        <div className="w-full mx-auto absolute top-[40px] h-[calc(100vh_-_40px)] overflow-y-auto px-[8px] min-w-[360px] pb-[15px] pt-[8px] flex flex-col gap-3">
+          {isMounted && <SummaryOfExamResult />}
           {isMounted && <CardResultWrapper cards={cards} />}
         </div>
       </M_Layout>
@@ -32,3 +37,45 @@ const ExamResult = () => {
 };
 
 export default ExamResult;
+
+const SummaryOfExamResult = () => {
+  const startedTime = useMemo(() => moment(new Date()).format("M.D hh:mm"), []);
+  const endedTime = useMemo(() => moment(new Date()).format("M.D hh:mm"), []);
+  const time = useMemo(
+    () =>
+      prettyMilliseconds(23456, {
+        colonNotation: true,
+        secondsDecimalDigits: 0,
+      }),
+    []
+  );
+
+  const displayTime = useCallback((time) => {
+    switch (time.length) {
+      case 4:
+        return "00:0" + time;
+      case 5:
+        return "00:" + time;
+      case 6:
+        return "00" + time;
+      case 7:
+        return "0" + time;
+      case 8:
+        return time;
+
+      default:
+        break;
+    }
+  }, []);
+
+  return (
+    <div className="grid w-full grid-cols-3 gap-4">
+      <BoxForSummaryOfMainPage title="시험 시작" content={startedTime} />
+      <BoxForSummaryOfMainPage title="시험 종료" content={endedTime} />
+      <BoxForSummaryOfMainPage
+        title="실제 학습 시간"
+        content={displayTime(time)}
+      />
+    </div>
+  );
+};

8dd54d7b39bda88a39d3e3782bfec4e891f89768
MrJooka <mr.jooka@gmail.com>
2022-02-08 19:11:38 +0900

    ing

--- pages/test.js
+++ pages/test.js
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const test = () => {
-  
   return (
     <table className="w-full table-fixed">
       <thead>
         <tr className="border-collapse border-y border-y-gray-200">
-          <th className="text-[1rem] bg-slate-100 w-[20%]">종류</th>
-          <th className="text-[1rem] bg-slate-100">{"'알겠음' 카드수"}</th>
-          <th className="text-[1rem] bg-slate-100">레벨 변동값 평균</th>
+          <th className="text-[1rem] bg-slate-100 w-[20%]"></th>
+          <th className="text-[1rem] bg-slate-100"></th>
+          <th className="text-[1rem] bg-slate-100"></th>
         </tr>
       </thead>
       <tbody>
@@ -16,18 +15,12 @@ const test = () => {
           <td className="text-[1rem] py-[4px] border-r border-collapse border-r-gray-200 text-center">
             Total
           </td>
-          <td className="text-[1rem] py-[4px] border-r border-collapse border-r-gray-200 text-center">
-            {changedLevel.total.count}
-          </td>
-          <td className="text-[1rem] py-[4px] text-center">
-            {averageOfGap > 0
-              ? "+" + averageOfGap
-              : averageOfGap < 0
-              ? "" + averageOfGap
-              : "-"}
-          </td>
+          <td className="text-[1rem] py-[4px] border-r border-collapse border-r-gray-200 text-center"></td>
+          <td className="text-[1rem] py-[4px] text-center"></td>
         </tr>
+      </tbody>
+    </table>
   );
-}
+};
 
 export default test;

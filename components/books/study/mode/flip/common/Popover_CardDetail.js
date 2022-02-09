4b35dbe6e12fb5b7352c7cff5c8e8dd2645cae5b
MrJooka <mr.jooka@gmail.com>
2022-02-09 17:46:21 +0900

    ing

--- components/books/study/mode/flip/common/Popover_CardDetail.js
+++ components/books/study/mode/flip/common/Popover_CardDetail.js
@@ -1,36 +1,69 @@
-import { Button, Popover } from "antd";
+import { gql, useApolloClient } from "@apollo/client";
+import { Button, Modal, Popover } from "antd";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { memo } from "react";
 
 const Popover_CardDetail = () => {
-  const card_details_session = JSON.parse(
-    sessionStorage.getItem("cardListStudying")
-  );
-  const currentSeq = Number(sessionStorage.getItem("card_seq"));
+  // const [visible, setVisible] = useState(false);
 
-  var cardLevel = card_details_session[currentSeq].studyStatus.levelCurrent;
-  var recentStudyTime =
-    card_details_session[currentSeq].studyStatus.recentStudyTime;
-  var recentSelection =
-    card_details_session[currentSeq].studyStatus.recentSelection;
+  // const client = useApolloClient();
+
+  // const card_details_session = JSON.parse(
+  //   sessionStorage.getItem("cardListStudying")
+  // );
+  // const currentSeq = Number(sessionStorage.getItem("card_seq"));
+
+  // const {
+  //   card_info: { cardtype, cardtype_id },
+  //   content: { mycontent_id, buycontent_id },
+  //   studyStatus: {
+  //     statusCurrent,
+  //     levelCurrent,
+  //     recentStudyTime,
+  //     recentStudyRatio,
+  //     totalStudyTimes,
+  //     totalStudyHour,
+  //   },
+  // } = card_details_session[currentSeq];
+
+  // const typeOfCard = ((_cardtype) => {
+  //   switch (_cardtype) {
+  //     case "read":
+  //       return "학습용 - 단면카드";
+  //     case "flip":
+  //       return "학습용 - 양면카드";
+  //     case "general":
+  //       return "비학습용 - 일반카드";
+  //     case "subject":
+  //       return "비학습용 - 제목카드";
+  //     case "share":
+  //       return "비학습용 - 공통지문카드";
+  //     default:
+  //       break;
+  //   }
+  // })(cardtype);
+
+  // let nameOfCardType;
+
+  // useEffect(() => {
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  //   nameOfCardType = client.readFragment({
+  //     id: `Cardtype:${cardtype_id}`,
+  //     fragment: gql`
+  //       fragment CardTypeNameFragment on Cardtype {
+  //         cardtype_info {
+  //           name
+  //         }
+  //       }
+  //     `,
+  //   });
+  // }, [nameOfCardType]);
+  // console.log({ nameOfCardType });
 
   return (
-    <Popover
-      content={
-        <div style={{ fontSize: "0.8rem" }}>
-          <div>현레벨 : {cardLevel}</div>
-          <div>최근학습시간 : {recentStudyTime}</div>
-          <div>마지막난이도 : {recentSelection}</div>
-          <div>기타등등... 카드 정보들</div>
-        </div>
-      }
-      placement="bottomRight"
-      title={
-        <>
-          <span style={{ fontSize: "0.8rem" }}>카드정보</span>
-        </>
-      }
-      trigger="click"
-    >
+    <>
       <Button
         size="small"
         style={{
@@ -43,11 +76,88 @@ const Popover_CardDetail = () => {
           padding: "2px",
           border: "none",
         }}
+        // onClick={() => setVisible(true)}
       >
         상세
       </Button>
-    </Popover>
+      {/* <Modal visible={visible} onCancel={() => setVisible(false)}>
+        <FlipCardDetail
+          typeOfCard={typeOfCard}
+          nameOfCardType={nameOfCardType}
+        />
+      </Modal> */}
+    </>
   );
 };
 
 export default Popover_CardDetail;
+
+// const FlipCardDetail = ({ typeOfCard, nameOfCardType }) => {
+//   return (
+//     <div className="flex flex-col gap-3 text-sm w-[23rem]">
+//       <div className="flex">
+//         <div className="min-w-[7rem]">카드타입</div>
+//         <div>{typeOfCard}</div>
+//       </div>
+
+//       <div className="flex">
+//         <div className="min-w-[7rem]">카드타입명</div>
+//         <div>{nameOfCardType}</div>
+//       </div>
+
+//       <div className="flex">
+//         <div className="min-w-[7rem]">행 구성</div>
+//         <div>
+//           <div className="flex">
+//             <div className="min-w-[4rem]">1면 보기</div>
+//             <div className="w-48 truncate">
+//               dkdkaksk아아ㅇㅇddddddddddd dadfs sㅇㅇㅇㅇ아dafs
+//             </div>
+//           </div>
+//           <div className="flex">
+//             <div className="min-w-[4rem]">1면 보기</div>
+//             <div className="w-48 truncate">ddd dadfs sㅇㅇㅇㅇ아dafs</div>
+//           </div>
+//           <div className="flex">
+//             <div className="min-w-[4rem]">1면 보기</div>
+//             <div className="w-48 truncate">dkdkakskㅇㅇㅇㅇ아dafs</div>
+//           </div>
+//           <div className="flex">
+//             <div className="min-w-[4rem]">1면 보기</div>
+//             <div className="w-48 truncate">dkdkaksk아아ㅇs</div>
+//           </div>
+//         </div>
+//       </div>
+
+//       <div className="flex">
+//         <div className="min-w-[7rem]">현재 상태</div>
+//         <div>학습전</div>
+//       </div>
+
+//       <div className="flex">
+//         <div className="min-w-[7rem]">레벨</div>
+//         <div>32 level</div>
+//       </div>
+
+//       <div className="flex">
+//         <div className="min-w-[7rem]">최근 뒤집기 시점</div>
+//         <div>2021년 2월 17일</div>
+//       </div>
+
+//       <div className="flex">
+//         <div className="min-w-[7rem]">최근 선택 난이도</div>
+//         <div>47.4</div>
+//       </div>
+
+//       <div className="flex">
+//         <div className="min-w-[7rem]">총 뒤집기 횟수</div>
+//         <div>3회</div>
+//       </div>
+
+//       <div className="flex">
+//         <div className="min-w-[7rem]">총 뒤집기 횟수</div>
+//         <div>24분 15초</div>
+//       </div>
+//     </div>
+//   );
+// };
